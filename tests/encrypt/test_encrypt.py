675d81576379ac910def5c1419aad18bd4c9289f
joabe <joabe9999pereira@gmail.com>
2023-06-10 15:31:10 -0300

    feat- requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("Hello World!", 2) == "!dlroW oll_eH"
+    assert encrypt_message("Joabe Pereira", 7) == "P ebaoJ_ariere"
+    assert encrypt_message("Saci Pererê", 6) == "êrere_P icaS"
+    assert encrypt_message("Redline", 10) == "enildeR"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("ivalid", "key")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(13, 9)
